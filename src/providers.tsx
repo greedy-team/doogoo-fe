ef7029d5bf42f1c41baea148a1ae6d6a871fe502
yoonjaehong26 <yjh0202261@gmail.com>
2026-02-21 23:42:02 +0900

    feat : React Query DevTools 설치 및 초기설정

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
 
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3, // 실패 시 3번 재시도
+      refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 리페치 비활성화
+    },
+  },
+});
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
