d5cd27015971453a79fadba8e2c34fadc04294c2
Johncakes <johnnygamesover9000@gmail.com>
2026-03-26 12:33:26 +0900

    fix: 미사용 depenedencies 제거

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChevronDownIcon } from 'lucide-react';
+// import { ChevronDownIcon } from 'lucide-react';
 import { Accordion as AccordionPrimitive } from 'radix-ui';
 
 import { cn } from '@/shared/lib/utils';
